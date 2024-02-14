7ab97baeb3ba3db27c78a643cd0cc5aeeabdacf4
Fabrício M. Damasceno <fabricio.damasceno@compasso.com.br>
2024-02-14 15:26:48 -0300

    Criando CRUD Minimal API

--- ECommerceLambda/src/ECommerceLambda/Controllers/ClienteController.cs
+++ ECommerceLambda/src/ECommerceLambda/Controllers/ClienteController.cs
@@ -1,3 +1,5 @@
+using ECommerceLambda.Models;
+using ECommerceLambda.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceLambda.Controllers
@@ -7,16 +9,40 @@ namespace ECommerceLambda.Controllers
     public class ClienteController : ControllerBase
     {
         private readonly ILogger<ClienteController> _logger;
+        private readonly IClienteRepository repository;
 
-        public ClienteController(ILogger<ClienteController> logger)
+        public ClienteController(ILogger<ClienteController> logger, IClienteRepository repository)
         {
             _logger = logger;
+            this.repository = repository;
         }
 
-        [HttpGet]
-        public IActionResult Index() 
+        [HttpPost]
+        public async Task<IActionResult> CriarCliente(Cliente cliente)
         {
+            await this.repository.Adicionar(cliente);
             return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> AtualizarCliente(Cliente cliente)
+        {
+            await this.repository.Atualizar(cliente);
+            return Ok();
+        }
+
+        [HttpDelete("{documento}")]
+        public async Task<IActionResult> DeletarCliente(string documento)
+        {
+            await this.repository.Deletar(documento);
+            return Ok();
+        }
+
+        [HttpGet("{documento}")]
+        public async Task<IActionResult> ObterClientePorDocumento(string documento) 
+        {
+            var cliente = await this.repository.Buscar(documento);
+            return Ok(cliente);
         }
     }
 }
